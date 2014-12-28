82cf865cc0ab74367d8b990d968a376e54115935
gdebasis <debforit@gmail.com>
2014-12-28 20:59:02 +0530

    Create bn_unicode.h

---
+++ bn_unicode.h
@@ -0,0 +1,84 @@
+#ifndef BN_UNICODE_H
+#define BN_UNICODE_H
+
+#define BN_RANGE_START  0x0980
+#define BN_RANGE_END    0x09FF
+
+#define bn_chandrabindu 0x0981
+#define bn_anusvara     0x0982
+#define bn_visarga      0x0983
+#define bn_a            0x0985
+#define bn_aa           0x0986
+#define bn_i            0x0987
+#define bn_ii           0x0988
+#define bn_u            0x0989
+#define bn_uu           0x098a
+#define bn_ri           0x098b
+#define bn_e            0x098f
+#define bn_ai           0x0990
+#define bn_o            0x0993
+#define bn_au           0x0994
+#define bn_k            0x0995
+#define bn_kh           0x0996
+#define bn_g            0x0997
+#define bn_gh           0x0998
+#define bn_ng           0x0999
+#define bn_ch           0x099a
+#define bn_chh          0x099b
+#define bn_j            0x099c
+#define bn_jh           0x099d
+#define bn_n            0x099e
+#define bn_tt           0x099f
+#define bn_tth          0x09a0
+#define bn_dd           0x09a1
+#define bn_ddh          0x09a2
+#define bn_mn           0x09a3
+#define bn_t            0x09a4
+#define bn_th           0x09a5
+#define bn_d            0x09a6
+#define bn_dh           0x09a7
+#define bn_dn           0x09a8
+#define bn_p            0x09aa
+#define bn_ph           0x09ab
+#define bn_b            0x09ac
+#define bn_bh           0x09ad
+#define bn_m            0x09ae
+#define bn_y            0x09df
+#define bn_J            0x09af
+#define bn_r            0x09b0
+#define bn_l            0x09b2
+#define bn_sh           0x09b6
+#define bn_ss           0x09b7
+#define bn_s            0x09b8
+#define bn_h            0x09b9
+/* matra */
+#define bn_AA           0x09be
+#define bn_I            0x09bf
+#define bn_II           0x09c0
+#define bn_U            0x09c1
+#define bn_UU           0x09c2
+#define bn_RI           0x09c3
+#define bn_E            0x09c7
+#define bn_AI           0x09c8
+#define bn_O            0x09cb
+#define bn_AU           0x09cc
+
+#define bn_hosh         0x09cd
+#define bn_nukta        0x09bc
+#define bn_virama       0x0964
+#define bn_khandata     0x09ce
+#define bn_rr           0x09dc // equivalent to 0x09A1 bn_dd + 0x09BC bn_nukta
+#define bn_rrh          0x09dd // equivalent to 0x09A2 bn_ddh + 0x09BC bn_nukta
+/* numerals */
+#define bn_zero         0x09e6
+#define bn_one          0x09e7
+#define bn_two          0x09e8
+#define bn_three        0x09e9
+#define bn_four         0x09ea
+#define bn_five         0x09eb
+#define bn_six          0x09ec
+#define bn_seven        0x09ed
+#define bn_eight        0x09ee
+#define bn_nine         0x09ef
+
+#endif
